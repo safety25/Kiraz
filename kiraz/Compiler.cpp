d24c128bb4f6d6d173e58d70dc9ffe2be74f409a
safety25 <alperensahin5207@gmail.com>
2024-11-30 14:16:04 +0300

    implemented  symboltable entries & compute module

--- kiraz/Compiler.cpp
+++ kiraz/Compiler.cpp
@@ -5,6 +5,7 @@
 #include <fmt/format.h>
 
 #include <resource/FILE_io_ki.h>
+#include "ast/Literal.h"
 
 Node::Ptr SymbolTable::s_module_ki;
 Node::Ptr SymbolTable::s_module_io;
@@ -101,9 +102,45 @@ int Compiler::compile(Node::Ptr root, std::ostream &ostr) {
     return 0;
 }
 
+#define IDENTIFIER(name) std::make_pair(name, std::make_shared<ast::Identifier>(curtoken))
+
+/*#define FUNCTION1(name, return_type, arg1_type, arg2_type) { \
+    std::make_pair(name, std::make_shared<ast::FuncNode>( \
+        std::make_shared<ast::Identifier>(Token::New<token::Identifier>(name)), \
+        std::make_shared<ast::FuncArgs>(std::vector<Node::Ptr>{ \
+            std::make_shared<ast::ArgNode>( \
+                std::make_shared<ast::Identifier>(Token::New<token::Identifier>(arg1_type)), \
+                std::make_shared<ast::Identifier>(Token::New<token::Identifier>(arg1_type)) \
+            ), \
+            std::make_shared<ast::ArgNode>( \
+                std::make_shared<ast::Identifier>(Token::New<token::Identifier>(arg2_type)), \
+                std::make_shared<ast::Identifier>(Token::New<token::Identifier>(arg2_type)) \
+            ) \
+        }), \
+        std::make_shared<ast::Identifier>(Token::New<token::Identifier>(return_type)) \
+    )) \
+}*/
+
+
+
 SymbolTable::SymbolTable()
-        : m_symbols({
-                  std::make_shared<Scope>(Scope::SymTab{}, ScopeType::Module, nullptr),
+        : m_symbols({std::make_shared<Scope>(
+                      Scope::SymTab{
+                        IDENTIFIER("String"),
+                        IDENTIFIER("Class"),
+                        IDENTIFIER("Void"),
+                        IDENTIFIER("Integer64"),
+                        IDENTIFIER("Module"),
+                        IDENTIFIER("Void"), 
+                        IDENTIFIER("Function"),
+
+                        IDENTIFIER("true"),
+                        IDENTIFIER("false"),
+
+                        //FUNCTION1("and","Boolean","Boolean","Boolean"),
+                        //FUNCTION1("or","Boolean","Boolean","Boolean"),
+                        //FUNCTION2("not","Boolean","Boolean"),
+                  }, ScopeType::Module, nullptr),
           }) {
     if (! s_module_io) {
         s_module_io = Compiler::current()->compile_module(FILE_io_ki);
--- kiraz/ast/testModule.h
+++ kiraz/ast/testModule.h
@@ -2,6 +2,7 @@
 #define KIRAZ_AST_MODULE_H
 
 #include <kiraz/Node.h>
+#include <kiraz/Compiler.h>
 
 namespace ast {
 
@@ -17,9 +18,46 @@ public:
 
     }
 
+    Node::Ptr compute_stmt_type(SymbolTable &st) override{
+        if(m_root){
+            assert(m_root -> is_stmt_list());
+            auto scope = st.enter_scope(ScopeType::Module, shared_from_this());
+            
+            for (const auto &stmt : static_cast<const NodeList &> (*m_root).get_list()){
+                if (auto ret = stmt ->add_to_symtab_forward(st)) {
+                    return ret;
+                }
+
+                if (auto ret = stmt -> add_to_symtab_forward(*m_symtab)) {
+                    return ret;
+
+                    }
+                }
+
+            for (const auto &stmt :static_cast<const NodeList &>(*m_root).get_list()){
+                if (auto ret = stmt -> add_to_symtab_ordered(st)) {
+                    return ret;
+
+                }
+
+                if (auto ret= stmt -> add_to_symtab_ordered(*m_symtab)){
+                    return ret;
+                }
+
+                if (auto ret= stmt ->compute_stmt_type(st)){
+                    return ret;
+                }
+            }
+        }
+        return nullptr;
+    }
+
 private:
     Node::Ptr m_root;
+    std::shared_ptr<SymbolTable> m_symtab;
 };
+
+
 
 } 
 
