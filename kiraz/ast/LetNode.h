470a28761c8aad45e38552c6df3d7ca4ad6e5e1b
Eren Eroglu <eroglueren9420@gmail.com>
2024-12-03 00:50:13 +0300

    added semantic checks for letnode: symboltable insertion and validation

--- kiraz/ast/LetNode.h
+++ kiraz/ast/LetNode.h
@@ -3,6 +3,7 @@
 
 #include <kiraz/Node.h>
 #include <string>
+#include <kiraz/Compiler.h>
 
 namespace ast {
 
@@ -14,16 +15,32 @@ public:
     std::string as_string() const override {
         std::string result = fmt::format("Let(n={}", m_name->as_string());
 
-    if (m_type) {
-        result += fmt::format(", t={}", m_type->as_string());
-    }
-    
-    if (m_initializer) {
-        result += fmt::format(", i={}", m_initializer->as_string());
+        if (m_type) {
+            result += fmt::format(", t={}", m_type->as_string());
+        }
+        
+        if (m_initializer) {
+            result += fmt::format(", i={}", m_initializer->as_string());
+        }
+
+        result += ")";
+        return result;
     }
 
-    result += ")";
-    return result;
+    Node::Ptr compute_stmt_type(SymbolTable &st) override {
+        set_cur_symtab(st.get_cur_symtab());
+
+        if (auto var_name = std::dynamic_pointer_cast<const ast::Identifier>(m_name)) {
+            if (st.get_symbol(var_name->get_name())) {
+                return set_error(fmt::format("Variable '{}' is already defined", var_name->get_name()));
+            }
+
+            st.add_symbol(var_name->get_name(), shared_from_this());
+        } else {
+            return set_error("LetNode name must be an identifier");
+        }
+
+        return shared_from_this(); 
     }
 
 private:
@@ -32,8 +49,6 @@ private:
     Node::Ptr m_initializer;  
 };
 
-
-
-}
+}  // namespace ast
 
 #endif // KIRAZ_AST_LET_NODE_H
